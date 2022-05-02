bab86b7293a3f7e3868db4b5eb06b1fff406e032
Tomio Kuboyama <viniciuskuboyama@gmail.com>
2022-05-02 17:42:29 -0300

    NavBar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './pages/login';
 import GlobalStyle from './styles/GlobalStyle';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyle />
       <button>Enviar</button>
