3781ce79c444fce7d93a98bc52a2a01856f17d18
Tomio Kuboyama <viniciuskuboyama@gmail.com>
2022-05-02 17:20:32 -0300

    Colors configured

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <>
       <Login />
       <GlobalStyle />
+      <button>Enviar</button>
     </>
   );
 }
---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export const primaryColor = '#C3073F';
+export const primaryDarkColor = '#1A1A1D';
+
+export const success = '#0197F';
+export const infoColor = '#0197F';
+export const errorColor = '#F2AF29';
+export const warningColor = '#F2AF29';
